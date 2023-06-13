468fe872823c52338fd64f1845351e6b43ec6c50
jongheonleee <qwefghnm1212@gmail.com>
2023-06-13 11:16:25 +0900

    로그인 처리하기 - (10) 직접 만든 세션 적용

---
+++ login/src/main/java/hello/login/domain/member/MemberRepository.java
@@ -0,0 +1,53 @@
+package hello.login.domain.member;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Slf4j
+@Repository
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        log.info("save : member = {}", member);
+        store.put(member.getId(), member);
+
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public Optional<Member> findByLoginId(String loginId) {
+//        List<Member> all = findAll();
+//
+//        for (Member m : all) {
+//            if (m.getLoginId().equals(loginId)) {
+//                return Optional.of(mx;
+//            }
+//        }
+//
+//        return Optional.empty();
+//
+//      위에 코드를 밑에와 같이 람다와 스트림을 활용하여 간단하게 나타낼 수 있음
+
+        return findAll().stream()
+                .filter(m -> m.getLoginId().equals(loginId))
+                .findFirst();
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
